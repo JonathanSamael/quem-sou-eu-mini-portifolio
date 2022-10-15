8cb43cfa2ba5d0fb79435fe7f27123923ac2d3dc
Jonathan Amaral <jonathansamael@live.com>
2022-10-14 22:10:17 -0300

    "Refatoração do código, adição de funções no JavaScript"

--- src/js/index.js
+++ src/js/index.js
@@ -26,20 +26,30 @@ abas.forEach(aba => {
             return;
         }
 
-        //Passo 3 - Quando o usuário clicar, desmarcar a aba selecionada
-        const abaSelecionada = document.querySelector(".aba.selecionado");
-        abaSelecionada.classList.remove("selecionado");
+        selecionarAba(aba)
 
-        //Passo 4 - Marcar a aba clicada como selecionado 
-        aba.classList.add("selecionado");
-
-        //Passo 5 - Esconder o conteúdo anterior
-        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
-        informacaoSelecionada.classList.remove("selecionado");
-
-        //Passo 6 - Mostrar o conteúdo da aba selecionada
-        //informacaoSelecionada.classList.add("selecionado")
-        
-
+        mostrarInformacoesDaAba(aba)
     });
 });
+
+function selecionarAba(aba) {
+    //Passo 3 - Quando o usuário clicar, desmarcar a aba selecionada
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    //Passo 4 - Marcar a aba clicada como selecionado 
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba) {
+    //Passo 5 - Esconder o conteúdo anterior
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    //Passo 6 - Mostrar o conteúdo da aba selecionada
+
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado")
+}
